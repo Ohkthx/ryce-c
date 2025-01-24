0470e50f7e982a171cc6dcaa094c00b502fa171d
ohkthx <ohkthx@proton.me>
2025-01-24 09:26:34 -0600

    Sequences that are less length than moving cursor used instead of moving cursor. Extracted shared functionality out of the TUI macros. Fixed numerous bugs with memory allocation for wide characters. Added TUI_HIDE_CURSOR to hide the cursor. Basic CMakeLists.txt added for building.

--- src/tui.h
+++ src/tui.h
@@ -4,6 +4,8 @@
 extern "C" {
 #endif
 
+#include <math.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,17 +17,15 @@ typedef wchar_t tuichar_t;
 typedef unsigned long tuipos_t;
 
 // Wide character functions.
-#define TUI_PRINTF(fmt, ...) wprintf((fmt), __VA_ARGS__)
-#define TUI_SNPRINTF(dst, n, fmt, ...) swprintf((dst), (n), (fmt), __VA_ARGS__)
+#define TUI_PRINTF(fmt, ...) wprintf((fmt), ##__VA_ARGS__)
+#define TUI_SNPRINTF(dst, n, fmt, ...) swprintf((dst), (n), (fmt), ##__VA_ARGS__)
 #define TUI_STRLEN(s) wcslen(s)
 #define TUI_MEMSET(dst, c, n) wmemset((dst), (wchar_t)(c), (n))
 #define TUI_MEMCPY(dst, src, n) wmemcpy((dst), (src), (n))
 
 // Wide character format strings.
-#define TUI_EMPTY_CHAR L' '
-#define TUI_NULL_CHAR L'\0'
-#define TUI_MOVE_ANSI L"\033[%lu;%luH"
-#define TUI_FMT_MOVE_ANSI L"%ls\033[%lu;%luH"
+#define TUI_LITERAL(s) L##s
+#define TUI_NUM_FMT TUI_LITERAL("%lu")
 
 #else
 
@@ -33,25 +33,41 @@ typedef char tuichar_t;
 typedef size_t tuipos_t;
 
 // Standard character functions.
-#define TUI_PRINTF(fmt, ...) printf((fmt), __VA_ARGS__)
-#define TUI_SNPRINTF(dst, n, fmt, ...) snprintf((dst), (n), (fmt), __VA_ARGS__)
+#define TUI_PRINTF(fmt, ...) printf((fmt), ##__VA_ARGS__)
+#define TUI_SNPRINTF(dst, n, fmt, ...) snprintf((dst), (n), (fmt), ##__VA_ARGS__)
 #define TUI_STRLEN(s) strlen(s)
 #define TUI_MEMSET(dst, c, n) memset((dst), (c), (n))
 #define TUI_MEMCPY(dst, src, n) memcpy((dst), (src), (n))
 
 // Standard character format strings.
-#define TUI_EMPTY_CHAR ' '
-#define TUI_NULL_CHAR '\0'
-#define TUI_MOVE_ANSI "\033[%zu;%zuH"
-#define TUI_FMT_MOVE_ANSI "%s\033[%zu;%zuH"
+#define TUI_LITERAL(s) s
+#define TUI_NUM_FMT TUI_LITERAL("%zu")
 
+#endif
+
+// String / Character constants and formatters.
+#define TUI_EMPTY_CHAR TUI_LITERAL(' ')
+#define TUI_NULL_CHAR TUI_LITERAL('\0')
+#define TUI_STR_FMT TUI_LITERAL("%s")
+
+// ANSI escape sequences.
+#define TUI_CSI TUI_LITERAL("\033[")
+#define TUI_MOVE_ANSI TUI_CSI TUI_NUM_FMT TUI_LITERAL(";") TUI_NUM_FMT TUI_LITERAL("H")
+#define TUI_HIDE_CURSOR_ANSI TUI_CSI TUI_LITERAL("?25l")
+#define TUI_UNHIDE_CURSOR_ANSI TUI_CSI TUI_LITERAL("?25h")
+
+#ifdef TUI_HIDE_CURSOR
+#define TUI_BUFFER_FMT TUI_STR_FMT
+#else
+#define TUI_BUFFER_FMT TUI_STR_FMT TUI_MOVE_ANSI
 #endif
 
 // Error Codes.
 const int TUI_ERR_INVALID_TUI = -1;           ///< Invalid TUI pointer.
 const int TUI_ERR_INVALID_PANE = -2;          ///< Invalid pane pointer.
-const int TUI_ERR_MOVE_BUFFER_OVERFLOW = -3;  ///< Move buffer overflow.
-const int TUI_ERR_WRITE_BUFFER_OVERFLOW = -4; ///< Write buffer overflow.
+const int TUI_ERR_INVALID_BUFFER = -3;        ///< Invalid buffer pointer.
+const int TUI_ERR_MOVE_BUFFER_OVERFLOW = -4;  ///< Move buffer overflow.
+const int TUI_ERR_WRITE_BUFFER_OVERFLOW = -5; ///< Write buffer overflow.
 
 /**
  * @brief Position within a 2D terminal.
@@ -60,6 +76,15 @@ typedef struct Position {
     size_t x; ///< X coordinate on terminal.
     size_t y; ///< Y coordinate on terminal.
 } Position;
+
+/**
+ * @brief Stores a buffer of characters.
+ */
+typedef struct SkipSequence {
+    int set;          ///< Flag to indicate if the buffer is set.
+    size_t start_idx; ///< Start index of the buffer.
+    size_t end_idx;   ///< End index of the buffer.
+} SkipSequence;
 
 /**
  * @brief Stores the contents of a terminal pane to be rendered.
@@ -81,8 +106,16 @@ typedef struct Tui {
     Pane *pane;                ///< Current pane.
     size_t max_length;         ///< Maximum length of the write_buffer.
     size_t write_length;       ///< Current length of the write_buffer.
-    tuichar_t *write_buffer; ///< Buffer that stores differences to be written.
+    tuichar_t *write_buffer;   ///< Buffer that stores differences to be written.
 } Tui;
+
+/**
+ * @brief Counts the number of digits in a number.
+ *
+ * @param num Number to count digits of.
+ * @return size_t Number of digits in the number.
+ */
+static inline size_t _TUI_digits(size_t num) { return num == 0 ? 1 : (size_t)log10(num) + 1; }
 
 /**
  * @brief Initializes a pane and fills its buffers with default values.
@@ -96,7 +129,8 @@ static inline Pane *_TUI_init_pane(size_t width, size_t height) {
         return NULL;
     }
 
-    Pane *pane = (Pane *)malloc(sizeof(Pane));
+    size_t total_size = sizeof(Pane) + (width * height) * sizeof(tuichar_t) * 2;
+    Pane *pane = (Pane *)malloc(total_size);
     if (!pane) {
         return NULL;
     }
@@ -104,19 +138,8 @@ static inline Pane *_TUI_init_pane(size_t width, size_t height) {
     pane->width = width;
     pane->height = height;
     pane->length = width * height;
-
-    pane->update = (tuichar_t *)malloc(pane->length * sizeof(tuichar_t));
-    if (!pane->update) {
-        free(pane);
-        return NULL;
-    }
-
-    pane->cache = (tuichar_t *)malloc(pane->length * sizeof(tuichar_t));
-    if (!pane->cache) {
-        free(pane->update);
-        free(pane);
-        return NULL;
-    }
+    pane->update = (tuichar_t *)(pane + 1);
+    pane->cache = pane->update + pane->length;
 
     // Fills the buffers with empty spaces.
     TUI_MEMSET(pane->update, TUI_EMPTY_CHAR, pane->length);
@@ -128,22 +151,7 @@ static inline Pane *_TUI_init_pane(size_t width, size_t height) {
 /**
  * @brief Checks if a pane is initialized correctly.
  */
-static inline int _TUI_is_init_pane(Pane *pane) {
-    return pane && pane->update && pane->cache;
-}
-
-/**
- * @brief Frees the memory allocated for a pane.
- */
-static inline void _TUI_free_pane(Pane *pane) {
-    if (!pane) {
-        return;
-    }
-
-    free(pane->update);
-    free(pane->cache);
-    free(pane);
-}
+static inline int _TUI_is_init_pane(Pane *pane) { return pane && pane->update && pane->cache; }
 
 /**
  * @brief Initializes a TUI with a pane and a buffer to store changes.
@@ -167,7 +175,7 @@ static inline Tui *TUI_init(size_t width, size_t height) {
     // Create the default pane.
     tui->pane = _TUI_init_pane(width, height);
     if (!tui->pane) {
-        printf("Failed to init pane.\n");
+        TUI_PRINTF("Failed to init pane.\n");
         free(tui);
         return NULL;
     }
@@ -175,18 +183,20 @@ static inline Tui *TUI_init(size_t width, size_t height) {
     // Create the buffer to store differences to be written.
     int default_length = (width * height) * 3;
     tui->max_length = default_length < 1024 ? 1024 : default_length;
-    tui->write_buffer =
-        (tuichar_t *)malloc(tui->max_length * sizeof(tuichar_t));
+    tui->write_buffer = (tuichar_t *)malloc(tui->max_length * sizeof(tuichar_t));
     if (!tui->write_buffer) {
-        _TUI_free_pane(tui->pane);
+        free(tui->pane);
         free(tui);
         return NULL;
     }
 
     tui->write_length = 0;
     TUI_MEMSET(tui->write_buffer, TUI_NULL_CHAR, tui->max_length);
-    TUI_MEMSET(tui->move_buffer, TUI_NULL_CHAR,
-               sizeof(tui->move_buffer) / sizeof(tuichar_t));
+    TUI_MEMSET(tui->move_buffer, TUI_NULL_CHAR, sizeof(tui->move_buffer) / sizeof(tuichar_t));
+
+#ifdef TUI_HIDE_CURSOR
+    TUI_PRINTF(TUI_HIDE_CURSOR_ANSI); // Hide the cursor.
+#endif
 
     return tui;
 }
@@ -199,9 +209,13 @@ static inline void TUI_free(Tui *tui) {
         return;
     }
 
-    _TUI_free_pane(tui->pane);
+    free(tui->pane);
     free(tui->write_buffer);
     free(tui);
+
+#ifdef TUI_HIDE_CURSOR
+    TUI_PRINTF(TUI_UNHIDE_CURSOR_ANSI); // Show the cursor.
+#endif
 }
 
 /**
@@ -232,8 +246,8 @@ static inline int _TUI_write_move(Tui *tui, size_t x, size_t y) {
     }
 
     // Write the move sequence to the buffer.
-    TUI_SNPRINTF(tui->move_buffer, sizeof(tui->move_buffer) / sizeof(tuichar_t),
-                 TUI_MOVE_ANSI, (tuipos_t)y + 1, (tuipos_t)x + 1);
+    TUI_SNPRINTF(tui->move_buffer, sizeof(tui->move_buffer) / sizeof(tuichar_t), TUI_MOVE_ANSI,
+                 (tuipos_t)y + 1, (tuipos_t)x + 1);
 
     size_t seq_len = TUI_STRLEN(tui->move_buffer);
     if (tui->write_length + seq_len >= tui->max_length) {
@@ -241,9 +255,56 @@ static inline int _TUI_write_move(Tui *tui, size_t x, size_t y) {
     }
 
     // Append the move sequence to the write buffer.
-    TUI_MEMCPY(tui->write_buffer + tui->write_length, tui->move_buffer,
-               seq_len);
+    TUI_MEMCPY(tui->write_buffer + tui->write_length, tui->move_buffer, seq_len);
     tui->write_length += seq_len;
+    return 0;
+}
+
+/**
+ * @brief Injects a move sequence or reprinted characters into the write buffer.
+ *
+ * @param tui TUI to write to.
+ * @param x X position to write to.
+ * @param y Y position to write to.
+ * @param skip Skip sequence to check.
+ * @return int 0 if successful, otherwise an error code.
+ */
+static inline int inject_sequence(Tui *tui, size_t x, size_t y, SkipSequence *skip) {
+    if (tui->cursor.y != y) {
+        // Move the cursor to the correct row.
+        int err_code = _TUI_write_move(tui, x, y);
+        if (err_code != 0) {
+            return err_code;
+        }
+
+        skip->set = 0; // Reset the skip sequence.
+        return 0;
+    } else if (!skip->set || tui->cursor.x == x) {
+        return 0;
+    }
+
+    size_t skipped_length = skip->end_idx - skip->start_idx;
+    size_t expected = _TUI_digits(x) + _TUI_digits(y) + 7;
+
+    if (skipped_length && skipped_length < expected) {
+        if (tui->write_length + skipped_length >= tui->max_length) {
+            // Not enough space in the write buffer.
+            return TUI_ERR_WRITE_BUFFER_OVERFLOW;
+        }
+
+        // Reprint skipped due to being cheaper than moving cursor.
+        TUI_MEMCPY(tui->write_buffer + tui->write_length, tui->pane->update + skip->start_idx,
+                   skipped_length);
+        tui->write_length += skipped_length;
+    } else {
+        // Move the cursor to the correct position.
+        int err_code = _TUI_write_move(tui, x, y);
+        if (err_code != 0) {
+            return err_code;
+        }
+    }
+
+    skip->set = 0; // Reset the skip sequence.
     return 0;
 }
 
@@ -262,24 +323,34 @@ static inline int TUI_render(Tui *tui) {
 
     // Reset the write and move sequence buffers.
     _TUI_softreset_tui(tui);
+    SkipSequence skip = {0, 0, 0};
+    size_t x = 1, y = 1;
 
     for (size_t i = 0; i < tui->pane->length; i++) {
-        size_t x = i % tui->pane->width; // X position in flattened buffer.
-        size_t y = i / tui->pane->width; // Y position in flattened buffer.
-
         if (tui->pane->update[i] == tui->pane->cache[i]) {
-            // No change, skip.
+            // No change, the character is skipable.
+            if (!skip.set) {
+                skip.start_idx = i;
+                skip.end_idx = i + 1;
+                skip.set = 1;
+            } else {
+                skip.end_idx++;
+            }
+
             continue;
         }
 
+        x = i % tui->pane->width; // X position in flattened buffer.
+        y = i / tui->pane->width; // Y position in flattened buffer.
+
+        // Inject move sequences or reprinted characters.
+        int seq_error = inject_sequence(tui, x, y, &skip);
+        if (seq_error != 0) {
+            return seq_error;
+        }
+
         // Check if we're next to the last written character.
-        if (tui->cursor.y != y || tui->cursor.x != x - 1) {
-            // Write the move cursor sequence.
-            int err_code = _TUI_write_move(tui, x, y);
-            if (err_code != 0) {
-                return err_code;
-            }
-        } else if (tui->write_length + 1 >= tui->max_length) {
+        if (tui->write_length + 1 >= tui->max_length) {
             // Exceeded capacity for the write buffer.
             return TUI_ERR_WRITE_BUFFER_OVERFLOW;
         }
@@ -292,15 +363,18 @@ static inline int TUI_render(Tui *tui) {
     }
 
     // Null-terminate for printf.
-    int index = tui->write_length < tui->max_length ? tui->write_length
-                                                    : tui->max_length - 1;
+    int index = tui->write_length < tui->max_length ? tui->write_length : tui->max_length - 1;
     tui->write_buffer[index] = TUI_NULL_CHAR;
 
+#ifdef TUI_HIDE_CURSOR
+    TUI_PRINTF(TUI_BUFFER_FMT, tui->write_buffer);
+#else
     // Render the differences and move the cursor to the bottom of the pane.
     tui->cursor.x = tui->pane->width;
     tui->cursor.y = tui->pane->height;
-    TUI_PRINTF(TUI_FMT_MOVE_ANSI, tui->write_buffer, (tuipos_t)tui->cursor.y,
+    TUI_PRINTF(TUI_BUFFER_FMT, tui->write_buffer, (tuipos_t)tui->cursor.y,
                (tuipos_t)tui->cursor.x);
+#endif
 
     fflush(stdout);
     return 0;
